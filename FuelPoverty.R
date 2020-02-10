a8ef1f2a95f00f3c5aec0d88c93b4db9512ce430
Peter Griffin <peterhughgriffin@gmail.com>
2020-02-10 13:41:07 +0000

    Functionalise Barplot and improve look of each plot

--- FuelPoverty.R
+++ FuelPoverty.R
@@ -10,24 +10,52 @@ HouseholdType = read.csv("HouseholdType.csv")  # read csv file
 
 # Calculate the percentages ----
 
+# Add in Percentage Share columns
+
+# Tenure
 Share_FP<-list(`Share_FuelPoor_pc`=100*Tenure$Number.fuel.poor..000s./Tenure$Number.fuel.poor..000s.[length(Tenure$Number.fuel.poor..000s.)])
 # Add it to the rest of the data
 Tenure_Exp<-cbind(Tenure,Share_FP)
 
+# Size
+Share_FP<-list(`Share_FuelPoor_pc`=100*HouseholdSize$Number.fuel.poor..000s./HouseholdSize$Number.fuel.poor..000s.[length(HouseholdSize$Number.fuel.poor..000s.)])
+# Add it to the rest of the data
+HouseholdSize_Exp<-cbind(HouseholdSize,Share_FP)
 
-Len<-nrow(Tenure_Exp)
-Wid<-ncol(Tenure_Exp)
-Temp<-Tenure_Exp[-Len,]
+# Type
+Share_FP<-list(`Share_FuelPoor_pc`=100*HouseholdType$Number.fuel.poor..000s./HouseholdType$Number.fuel.poor..000s.[length(HouseholdType$Number.fuel.poor..000s.)])
+# Add it to the rest of the data
+HouseholdType_Exp<-cbind(HouseholdType,Share_FP)
 
-p<-barplot(Temp$Share_FuelPoor_pc[order(Temp$Share_FuelPoor_pc,decreasing = TRUE)],
-           main="Average CO2 Emissions by Household Type",
-           ylab="Emissions (kg/m2/year)",
-           col=topo.colors(4))
-axis(1,labels=FALSE,at=seq(0.05,8.2,1.22))
-text(p+0.5, -3,
-     labels=Temp[,1],
-     srt=20, adj=1, xpd=TRUE)
-text(p+0.5, 3+Temp$Share_FuelPoor_pc[order(Temp$Share_FuelPoor_pc,decreasing = TRUE)],
-     labels=paste(round(Temp$Share_FuelPoor_pc[order(Temp$Share_FuelPoor_pc,decreasing = TRUE)],1),'%'),
-     srt=0, adj=1, xpd=TRUE)
+# Plotting ----
 
+#Plot Function
+
+Plot_FP<-function(Table,Lab_Title,Lab_y,Lab_tilt,Align){
+  Len<-nrow(Table)
+  Wid<-ncol(Table)
+  Temp<-Table[-Len,]
+  
+  
+  x11(width=9, height=6, pointsize=12)
+  par(mar=c(10,5,2,5))
+  
+  p<-barplot(Temp$Share_FuelPoor_pc[order(Temp$Share_FuelPoor_pc,decreasing = TRUE)],
+             width=Wid/Len,
+             main=Lab_Title,
+             ylab=Lab_y,
+             col=topo.colors(4),
+             ylim=c(0,max(Temp$Share_FuelPoor_pc)+10),
+             xlim=c(0,Wid))
+  axis(1,labels=FALSE,at=p)
+  text(p+Align, -3,
+       labels=Temp[,1],
+       srt=Lab_tilt, adj=1, xpd=TRUE)
+  text(p+0.5, 3+Temp$Share_FuelPoor_pc[order(Temp$Share_FuelPoor_pc,decreasing = TRUE)],
+       labels=paste(round(Temp$Share_FuelPoor_pc[order(Temp$Share_FuelPoor_pc,decreasing = TRUE)],1),'%'),
+       srt=0, adj=1, xpd=TRUE)
+}
+
+Plot_FP(Tenure_Exp,"Fuel Poverty by Tenure","Fuel Poor (%)",15,0.5)
+Plot_FP(HouseholdSize_Exp,"Fuel Poverty by Household Size","Fuel Poor (%)",0,0)
+Plot_FP(HouseholdType_Exp,"Fuel Poverty by Household Type","Fuel Poor (%)",15,0.5)
