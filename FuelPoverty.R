444ef0a05674ee50b3fe0f9c1e7b39ea5bbae61a
Peter Griffin <peterhughgriffin@gmail.com>
2020-02-11 14:40:47 +0000

    Improve plotting functionality

--- FuelPoverty.R
+++ FuelPoverty.R
@@ -28,39 +28,48 @@ HouseholdType_Exp<-cbind(HouseholdType,Share_FP)
 
 #Plot Function
 
-Plot_FP<-function(Table,Lab_Title,Lab_x,Lab_y,Lab_tilt,Align,Order){
+Plot_FP<-function(Table,Column,Lab_Title,Lab_x,Lab_y,Lab_tilt,Align,Order,LabStr){
   Len<-nrow(Table)
   Wid<-ncol(Table)
   Temp<-Table[-Len,]
   if (Order){
-    Temp<-Temp[order(Temp$Share_FuelPoor_pc,decreasing = TRUE),]
+    Temp<-Temp[order(Temp[Column],decreasing = TRUE),]
   }
   
   x11(width=9, height=6, pointsize=12)
   par(mar=c(10,5,2,5))
   
-  p<-barplot(Temp$Share_FuelPoor_pc,
+  p<-barplot(Temp[,Column],
              width=Wid/Len,
              main=Lab_Title,
              ylab=Lab_y,
              xlab=Lab_x,
              col=heat.colors(Len),
-             ylim=c(0,max(Temp$Share_FuelPoor_pc)+10),
+             ylim=c(0,max(Temp[,Column])+max(Temp[,Column])/5),
              xlim=c(0,Wid),
              cex.axis=2,
              cex.lab=2)
   axis(1,labels=FALSE,at=p)
-  text(p+Align, -3,
+  text(p+Align, -max(Temp[,Column])/12,
        labels=Temp[,1],
        srt=Lab_tilt, adj=1, xpd=TRUE,cex=2)
-  text(p+0.5, 3+Temp$Share_FuelPoor_pc,
-       labels=paste(round(Temp$Share_FuelPoor_pc,1),'%'),
+  text(p+0.6, max(Temp[,Column])/12+Temp[,Column],
+       labels=paste(round(Temp[,Column],1),LabStr),
        srt=0, adj=1, xpd=TRUE,cex=2)
 }
 
-Plot_FP(Tenure_Exp,"Fuel Poverty by Tenure","","Proportion Fuel Poor (%)",7,0.6,TRUE)
-Plot_FP(HouseholdSize_Exp,"Fuel Poverty by Household Size","Household Size","Proportion Fuel Poor (%)",0,0.07,FALSE)
-# Plot_FP(HouseholdType_Exp,"Fuel Poverty by Household Type","","Proportion Fuel Poor (%)",15,0.5,TRUE)
 
+# # Percentage Fuel Poor
+# Plot_FP(Tenure_Exp,6,"Fuel Poverty by Tenure","","Proportion Fuel Poor (%)",7,0.6,TRUE,'%')
+# Plot_FP(HouseholdSize_Exp,6,"Fuel Poverty by Household Size","Household Size","Proportion Fuel Poor (%)",0,0.07,FALSE,'%')
+# Plot_FP(HouseholdType_Exp,6,"Fuel Poverty by Household Type","","Proportion Fuel Poor (%)",15,0.5,TRUE,'%')
 
+# Number Fuel Poor
+Plot_FP(Tenure_Exp,5,"Fuel Poverty by Tenure","","Number of Fuel Poor",7,0.6,TRUE,'')
+Plot_FP(HouseholdSize_Exp,5,"Fuel Poverty by Household Size","Household Size","Number of Fuel Poor",0,0.07,FALSE,'')
+# Plot_FP(HouseholdType_Exp,5,"Fuel Poverty by Household Type","","Number of Fuel Poor",15,0.5,TRUE,'')
 
+# Percentage Fuel Poor
+Plot_FP(Tenure_Exp,3,"Fuel Poverty by Tenure","","Proportion Fuel Poor (%)",7,0.6,TRUE,'%')
+Plot_FP(HouseholdSize_Exp,3,"Fuel Poverty by Household Size","Household Size","Proportion Fuel Poor (%)",0,0.07,FALSE,'%')
+# Plot_FP(HouseholdType_Exp,3,"Fuel Poverty by Household Type","","Proportion Fuel Poor (%)",15,0.5,TRUE,'%')
